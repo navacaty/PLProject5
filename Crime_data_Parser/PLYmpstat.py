dc80bd0536a1ea85e5e44827e6c74b7c06a97470
Brandon Bartos <brandon_b40@yahoo.com>
2016-04-13 23:17:50 -0500

    Final update

--- Crime_data_Parser/PLYmpstat.py
+++ Crime_data_Parser/PLYmpstat.py
@@ -1,18 +1,19 @@
-""" This will parse the data from the unix command echo "Header1 is this~Header2 and that~~Data 1.0~Data 2.0" | tr "~" "\n"
-which is:
-Header1 is this
-Header2 and that
+"""
+##### To run the parser do the following in a terminal window: cat crime.out | python PlyMpstat.py #####
 
-Data 1.0
-Data 2.0
+This program will run through data collected from San Fransisco's crime registry from Wednesday, 5-13-15 and
+print out the time and type of offense
+
+Brandon Bartos, Caty Nava, Tristan Miranda##
+
 """
 
-tokens = ('INTEGER','STRING','DATE')
+tokens = ('HEADER1', 'INTEGER', 'DATE', 'STRING')
 literals = ['.', ':','-']
 
 # Tokens
+t_HEADER1  = r'^Header1[ -~]+$'
 t_DATE   = r'Dates.*$'
-t_STRING = r'[A-Z_].*$'
 
 def t_INTEGER(t):
     r'\d+'
@@ -23,17 +24,18 @@ def t_INTEGER(t):
         t.value = 0
     return t
 
-'''def t_STRING(t):
-    r'\d+'
+def t_STRING(t):
+    r'\w+'
     try:
         t.value = str(t.value)
     except ValueError:
-        print("String value too large %d", t.value)
-        t.value = 0
-    return t'''
+        print("String problem %s", t.value)
+        t.value = ""
+    return t
+
 
 # Ignored characters
-t_ignore = " \t"
+t_ignore = " \r" + " \t"
 
 def t_newline(t):
     r'\n+'
@@ -53,42 +55,50 @@ global time_step
 time_step = 0
 
 def p_start(t):
-    '''start : empty
+    '''start : HEADER1
+             | empty
              | data
              | date
              | DATE
              | offense
-             | STRING
-    '''
-    print t[1]
+             | day
 
-def p_time(t):
-    'time : INTEGER ":" INTEGER'
-    t[0] = str(t[1]) + str(t[2]) + str(t[3])
+    '''
 
 def p_date(t):
     'date : INTEGER "-" INTEGER "-" INTEGER'
     t[0] = str(t[1]) + str(t[2]) + str(t[3]) + str(t[4]) + str(t[5])
 
+def p_time(t):
+    'time : INTEGER ":" INTEGER'
+    t[0] = str(t[1]) + str(t[2]) + str(t[3])
+
+
 def p_offense(t):
     'offense : STRING "-" STRING'
-    t[0] = str(t[1]) + str(t[2]) +str(t[3])
+    t[0] = str(t[1]) + str(t[2]) + str(t[3])
+
+def p_day(t):
+    'day : STRING'
+    t[0] = str(t[1])
 
 
 def p_data(t):
-    'data : date time offense STRING'
-    print "Saw a Data Line With : " + str(t[2])
+    'data : date time offense day'
+    print "Time/Offense Type: " + str(t[2]) + '/' +str(t[3])
 
 
 def p_empty(t):
-    'empty : '
+    'empty :    '
     pass
+
 
 def p_error(t):
     if t == None:
         print("Syntax error at '%s'" % t)
     else:
         print("Syntax error at '%s'" % t.value)
+
 
 import ply.yacc as yacc   # ply.yacc comes from the ply folder in the PLY download.
 parser = yacc.yacc()
@@ -100,4 +110,4 @@ while True:
         break
     parser.parse(s)
 
-# To run the parser do the following in a terminal window: echo "Header1 is this~Header2 and that~~Data 1.0~Data 2.0" | tr "~" "\n" | grep -v '^\s*$' | python PLYstarter.py | sed "s/_~_/ which is a float./"
+##### To run the parser do the following in a terminal window: cat crime.out | python PlyMpstat.py #####
---
+++ README.txt
@@ -0,0 +1,3 @@
+Part 1 - In the ListComprehension folder, run “ListComprehension.java” in src>nava
+
+Part 2 - In the Crime_data_Parse folder, run “PLYmpstat.py” and copy the provided command into the terminal
